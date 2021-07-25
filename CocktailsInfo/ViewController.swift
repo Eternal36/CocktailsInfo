967a985671a07ddd35766a24a1955372d2a0958e
Dmitriy Belkov <belkov671@yandex.ru>
2021-07-25 16:17:15 +0300

    Initial Commit

---
+++ CocktailsInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocktailsInfo
+//
+//  Created by Дмитрий Бельков on 25.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
